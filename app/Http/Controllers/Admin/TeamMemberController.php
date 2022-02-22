81310ffd7f95638d91a6e8f28d284b9868ef6cfa
coderkhayrul <coderkhayrul@gmail.com>
2022-02-23 03:19:23 +0600

    Team Mamber Create Function Add

--- app/Http/Controllers/Admin/TeamMemberController.php
+++ app/Http/Controllers/Admin/TeamMemberController.php
@@ -96,7 +96,7 @@ class TeamMemberController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($slug)
     {
         //
     }
