4647ee4b43f6dd2c6f0379122428e246a16aa832
coderkhayrul <coderkhayrul@gmail.com>
2022-02-25 23:12:29 +0600

    Project Update And Delete Freature Add

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -14,6 +14,10 @@ use Intervention\Image\ImageManagerStatic as Image;
 
 class ProjectController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
@@ -113,7 +117,9 @@ class ProjectController extends Controller
      */
     public function edit($slug)
     {
-        //
+        $categories = ProjectCategory::where('procate_status', 1)->orderBy('procate_id', 'DESC')->get();
+        $data = Project::where('project_status', 1)->where('project_slug', $slug)->firstOrFail();
+        return view('admin.project.edit', compact('data', 'categories'));
     }
 
     /**
@@ -123,9 +129,51 @@ class ProjectController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $slug)
     {
-        //
+        // Insert Project Validation
+        $this->validate($request, [
+            'project_title' => 'required',
+            'procate_id' => 'required',
+        ], [
+            'project_title.required' => 'enter your project title',
+            'procate_id.required' => 'select your project category',
+        ]);
+
+        // Project Update With Get Id
+        $slug = 'P' . uniqid();
+        $editor = Auth::user()->id;
+        $id = $request['project_id'];
+
+        $update = Project::where('project_status', 1)->where('project_id', $id)->update([
+            'project_title' => $request['project_title'],
+            'procate_id' => $request['procate_id'],
+            'project_url' => $request['project_url'],
+            'project_order' => $request['project_order'],
+            'project_remarks' => $request['project_remarks'],
+            'project_editor' => $editor,
+            'updated_at' => Carbon::now()->toDateTimeString(),
+        ]);
+
+        // Project Image Upload
+        if ($request->hasFile('project_image')) {
+            $image = $request->file('project_image');
+            $imageName = $id . time() . '.' . $image->getClientOriginalExtension();
+            Image::make($image)->save('uploads/project/' . $imageName);
+
+            Project::where('project_id', $id)->update([
+                'project_image' => $imageName,
+                'updated_at' => Carbon::now()->toDateTimeString(),
+            ]);
+        }
+
+        if ($update) {
+            Session::flash('success', 'Project Update successfully');
+            return redirect()->back();
+        } else {
+            Session::flash('error', 'Project Update Failed!');
+            return redirect()->back();
+        }
     }
 
     /**
@@ -134,8 +182,17 @@ class ProjectController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Request $request, $slug)
     {
-        //
+        $id = $request['delete_data'];
+        $delete = Project::where('project_id', $id)->delete();
+
+        if ($delete) {
+            Session::flash('success', 'Project Delete successfully');
+            return redirect()->back();
+        } else {
+            Session::flash('error', 'Project Delete Failed!');
+            return redirect()->back();
+        }
     }
 }
--- resources/views/admin/project/edit.blade.php
+++ resources/views/admin/project/edit.blade.php
@@ -1,1 +1,162 @@
+@extends('layouts.admin-layout')
+@section('admin-content')
+    <!-- start page title -->
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box">
+                <div class="page-title-right">
+                    <form class="d-flex">
+                        <div class="input-group">
+                        </div>
+                        <a href="javascript: void(0);" class="btn btn-primary ms-2">
+                            <i class="mdi mdi-autorenew"></i>
+                        </a>
+                        <a href="javascript: void(0);" class="btn btn-primary ms-1">
+                            <i class="mdi mdi-filter-variant"></i>
+                        </a>
+                    </form>
+                </div>
+                <h4 class="page-title">Project</h4>
+            </div>
+        </div>
+    </div>
 
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <form class="form-horizontal" action="{{ route('project.update', $data->project_slug) }}" method="POST"
+                    enctype="multipart/form-data">
+                    @csrf
+                    @method('PUT')
+                    <input type="hidden" name="project_id" value="{{ $data->project_id }}" />
+                    <div class="card-header d-flex justify-content-between">
+                        <strong class="fs-4"> <i class="uil-briefcase"></i> Update Project</strong>
+                        <a href="{{ route('project.index') }}" class="btn btn-dark btn-sm"><i class="uil-briefcase"></i>
+                            All Project</a>
+                    </div>
+                    <div class="card-body">
+                        <div class="row mb-3 mt-3">
+                            <label for="name" class="col-3 col-form-label">Project Title <strong
+                                    class="text-danger">*</strong></label>
+                            <div class="col-9">
+                                <input type="text" value="{{ $data['project_title'] }}"
+                                    class="form-control @error('project_title') is-invalid @enderror" id="project_title"
+                                    name="project_title" placeholder="Project Title">
+                                @error('project_title')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="row mb-3 mt-3">
+                            <label for="name" class="col-3 col-form-label">Url <strong
+                                    class="text-danger">*</strong></label>
+                            <div class="col-9">
+                                <input type="text" value="{{ $data['project_url'] }}"
+                                    class="form-control @error('project_url') is-invalid @enderror" id="project_url"
+                                    name="project_url" placeholder="www.example.com">
+                                @error('project_url')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="email" class="col-3 col-form-label">Order By <strong
+                                    class="text-danger">*</strong></label>
+                            <div class="col-9">
+                                <input type="number" value="{{ $data['project_order'] }}"
+                                    class="form-control @error('project_order') is-invalid @enderror" id="project_order"
+                                    name="project_order" placeholder="Partner Order">
+                                @error('project_order')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="role" class="col-3 col-form-label">Selete Project Category <strong
+                                    class="text-danger">*</strong></label>
+                            <div class="col-9">
+                                <select class="form-select mb-3 @error('procate_id') is-invalid @enderror"
+                                    name="procate_id">
+                                    <option disabled selected>Select Your Category</option>
+                                    @foreach ($categories as $category)
+                                        <option value="{{ $category->procate_id }}"
+                                            {{ $category->procate_id == $data->procate_id ? ' selected ' : '' }}>
+                                            {{ $category->procate_name }}</option>
+                                    @endforeach
+                                </select>
+                                @error('procate_id')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="feedback" class="col-3 col-form-label">Remarks <strong
+                                    class="text-danger">*</strong></label>
+                            <div class="col-9">
+                                <textarea class="form-control @error('project_remarks') is-invalid @enderror"
+                                    placeholder="Leave a feedback here" name="project_remarks" id="project_remarks"
+                                    style="height: 100px">{{ $data['project_remarks'] }}</textarea>
+                                @error('project_remarks')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="image" class="col-3 col-form-label">Image Upload</label>
+                            <div class="col-6">
+                                <input type="file" id="example-fileinput" name="project_image"
+                                    class="form-control @error('project_image') is-invalid @enderror">
+                                @error('project_image')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="col-3 text-center">
+                                @if ($data['project_image'])
+                                    <img id="preview-image" src="{{ asset('uploads/project/' . $data->project_image) }}"
+                                        alt="image" class="img-fluid rounded" width="80px" />
+                                @else
+                                    <img id="preview-image" src="{{ asset('uploads/noimage.png') }}" alt="image"
+                                        class="img-fluid rounded" width="80px" />
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card-footer bg-dark row justify-content-md-center">
+                        <div class="col col-lg-3">
+                            <button type="submit" class="btn btn-primary btn-sm"><i class="uil-sync me-1"></i>
+                                <span>Project Update</span> </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <script type="text/javascript">
+        $('#example-fileinput').change(function() {
+            let reader = new FileReader();
+            reader.onload = (e) => {
+                $('#preview-image').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(this.files[0]);
+
+        });
+    </script>
+@endsection
